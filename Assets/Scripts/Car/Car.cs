1a492c3237534f06c787032cbf13f302eed9e2a7
gnda <kevin.nandacoumar@gmail.com>
2020-02-07 09:36:34 +0100

    Ai done + circuits

---
+++ Assets/Scripts/Car/Car.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] public Material[] Palettes;
+
+    private void Start()
+    {
+        // Randomize car color
+        if (GetComponent<Player>() == null)
+            transform.Find("SkyCar").Find("SkyCarBody").
+                    GetComponent<MeshRenderer>().sharedMaterial =
+                        Palettes[Random.Range(0, Palettes.Length)];
+    }
+}
